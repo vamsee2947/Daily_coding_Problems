4330ff49eb9342483e9c69887248acb429a27d04
vamsee2947 <44166241+vamsee2947@users.noreply.github.com>
2022-05-27 09:13:00 +0530

    Create DCP_26_05_2022.java

---
+++ DCP_26_05_2022.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main
+{
+    public static void main (String[] args)
+    {
+        int [] a = {34, -50, 42, 14, -5, 86]};
+        System.out.println("Maximum contiguous sum is " + maxsum(a));
+    }
+ 
+    static int maxsum(int a[])
+    {
+        int size = a.length;
+        int max = Integer.MIN_VALUE;
+        int max_end = 0;
+ 
+        for (int i = 0; i < size; i++)
+        {
+            max_end = max_end + a[i];
+            if (max < max_end)
+                max = max_end;
+            if (max_end < 0)
+                max_end = 0;
+        }
+        return max;
+    }
+}
