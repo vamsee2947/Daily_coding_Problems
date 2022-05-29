576f291f6cd1cd50c59fef413eb72aea39211127
vamsee2947 <44166241+vamsee2947@users.noreply.github.com>
2022-05-29 11:50:58 +0530

    Update DCP_17_05_2022.py

--- DCP_17_05_2022.py
+++ DCP_17_05_2022.py
@@ -1,3 +1,7 @@
+
+#Given an array of integers where every integer occurs three times except for one integer,
+#which only occurs once, find and return the non-duplicated integer.
+
 def singleNumber(nums):
     return (3 * sum(set(nums)) - sum(nums)) / 2
 
