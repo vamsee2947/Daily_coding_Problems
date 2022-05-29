99fb08700bbd7ea5dd03c1370bdafd160b393829
vamsee2947 <44166241+vamsee2947@users.noreply.github.com>
2022-05-29 11:43:41 +0530

    Create DCP_28_05_2022.java

---
+++ DCP_28_05_2022.java
@@ -0,0 +1,34 @@
+/*Given a function that generates perfectly random numbers between 1 and k (inclusive), 
+where k is an input, write a function that shuffles a deck of cards represented as an array using only swaps.*/
+
+
+import java.util.Random;
+import java.util.Arrays;
+public class Main
+{
+	
+	static void randomize( int arr[], int n)
+	{
+		
+		Random r = new Random();
+		
+		for (int i = n-1; i > 0; i--) {
+			
+			int j = r.nextInt(i+1);
+			
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+		int n = arr.length;
+		randomize (arr, n);
+	}
+}
+
